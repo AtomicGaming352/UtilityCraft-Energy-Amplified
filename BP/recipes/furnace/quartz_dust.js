82f6cebcbe94abac7ed510b54a1527ef9a497de5
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-02-15 14:35:28 +0800

    Create quartz_dust.js

---
+++ BP/recipes/furnace/quartz_dust.js
@@ -0,0 +1,11 @@
+{
+    "format_version": "1.21.40",
+    "minecraft:recipe_furnace": {
+        "description": {
+            "identifier": "utilitycraft:is_silicon"
+        },
+        "tags": ["furnace", "blast_furnace"],
+        "input": "utilitycraft:nether_quartz_dust",
+        "output": "ae2:silicon"
+    }
+}
