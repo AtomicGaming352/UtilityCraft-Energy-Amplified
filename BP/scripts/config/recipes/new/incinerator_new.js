3d1c0c4bcf0e42292ff6aa85cd65bb4bda134c2e
AtomicGaming352 <atomicgaming55yt@gmail.com>
2026-02-15 14:29:17 +0800

    Update incinerator_new.js

--- BP/scripts/config/recipes/new/incinerator_new.js
+++ BP/scripts/config/recipes/new/incinerator_new.js
@@ -1,7 +1,7 @@
 import { system, world } from "@minecraft/server";
 world.afterEvents.worldLoad.subscribe(() => {
     const newRecipes = {
-        "minecraft:redstone": { output: "utilitycraft:flux" },
+        "minecraft:redstone": { output: "utilitycraft:flux", amount: 1, required:, 16 },
     };
     system.sendScriptEvent("utilitycraft:register_furnace_recipe", JSON.stringify(newRecipes));
 })
