ba970c64249321bb27f008561d6de1ce75369758
Kauziin_ <74920731+Kauziin@users.noreply.github.com>
2026-02-20 11:33:14 -0300

    refactor(bp/scripts): modularize doriosapi and insight injectors

---
+++ BP/scripts/doriosAPI/config.js
@@ -0,0 +1,19 @@
+export const addonData = {
+    name: "UtilityCraft: Energy Amplified",
+    author: "NeoUtility Studios",
+    identifier: "utilitycraft_energy_amplified",
+    version: "1.0.0",
+    dependencies: {
+        "utilitycraft": {
+            name: "UtilityCraft",
+            version: "3.3.5"
+        }
+    }
+}
+
+import './API.js'
+import './dependencyChecker.js'
+import './blockClass.js'
+import './playerClass.js'
+import './itemStackClass.js'
+import './entityClass.js'
--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,3 +1,4 @@
 import './doriosAPI/main.js'
 import './machinery/main.js'
 import './config/main.js'
+import './insight_injectors.js'
